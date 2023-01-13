c79de1b1cb9df5e61013d50be9e50889d51a6ac5
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2023-01-13 18:48:40 +0100

    propper RTX

--- src/java/othello/game/GameScene.java
+++ src/java/othello/game/GameScene.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 
 // Manages game Pane and events
 
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -15,8 +14,12 @@ import othello.ui.SceneProvider;
 
 public class GameScene extends SceneProvider {
 	public boolean rtxOn = false;
-	public BorderPane root;
+	public BorderPane basicPane;
+	public BorderPane advancedPane;
 	public Board2D board;
+
+	public othello.game.board.basic.BoardScene basicBoard;
+	public othello.game.board.advanced.BoardScene advancedBoard;
 
     public GameScene(SceneManager manager, Board2D board) {
         super(manager, "GameScene");
@@ -29,31 +32,36 @@ public class GameScene extends SceneProvider {
 		rtxButton.setOnAction(this::toggleRTX);
 		grid.add(rtxButton, 1, 0);
 
-        Scene boardScene = new othello.game.board.basic.BoardScene(manager, this.board).getScene();
-		this.root = new BorderPane(); 
-		this.root.setCenter(boardScene.getRoot());
-		this.root.setTop(grid);
-		boardScene.setRoot(this.root);
-		this.setScene(boardScene);
+        this.basicBoard = new othello.game.board.basic.BoardScene(manager, this.board);	
+
+		this.basicPane = new BorderPane(); 
+		this.basicPane.setCenter(basicBoard.getRoot());
+		this.basicPane.setTop(grid);
+		this.basicBoard.getScene().setRoot(this.basicPane);
+
+		grid = new GridPane();
+		grid.add(new GoBackOnce(manager).getRoot(), 0, 0);
+		rtxButton = new Button("RTX");
+		rtxButton.setOnAction(this::toggleRTX);
+		grid.add(rtxButton, 1, 0);
+
+		this.advancedBoard = new othello.game.board.advanced.BoardScene(manager, this.board);
+
+		this.advancedPane = new BorderPane();
+		this.advancedPane.setCenter(advancedBoard.getRoot());
+		this.advancedPane.setTop(grid);
+		this.advancedBoard.getScene().setRoot(this.advancedPane);
+
+		this.setScene(this.basicBoard.getScene());
 	}
 
 	public void toggleRTX(ActionEvent event) {
 		this.rtxOn = !this.rtxOn;
 
 		if (this.rtxOn) {
-			Scene boardScene = new othello.game.board.advanced.BoardScene(
-				this.getSceneManager(),
-				this.board
-			).getScene();
-			
-			this.root.setCenter(boardScene.getRoot());
+			this.getSceneManager().setActiveScene(this.advancedBoard.getScene());
 		} else {
-			Scene boardScene = new othello.game.board.basic.BoardScene(
-				this.getSceneManager(),
-				this.board
-			).getScene();
-			
-			this.root.setCenter(boardScene.getRoot());
+			this.getSceneManager().setActiveScene(this.basicBoard.getScene());
 		}
 	}
 }
