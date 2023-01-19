126074ca741eb0feff2972c8dd2ee273588a0604
Javad Asgari Shafique <javadshafique@hotmail.com>
2023-01-19 23:35:16 +0100

    Normalize doubles to ints and refactor hover styles

--- src/java/othello/components/ui/FancyButton.java
+++ src/java/othello/components/ui/FancyButton.java
@@ -7,8 +7,8 @@ public class FancyButton extends Button {
     public FancyButton(String text, Color color) {
         super(text);
 
-        String colorString = String.format("rgba(%f, %f, %f, %f)", color.getRed() * 255, color.getGreen() * 255, color.getBlue() * 255, color.getOpacity());
-        String hoverColorString = String.format("rgba(%f, %f, %f, %f)", color.getRed() * 255 + 20, color.getGreen() * 255 + 20, color.getBlue() * 255 + 20, color.getOpacity() - 0.2);
+        String colorString = String.format("rgba(%d, %d, %d, %d)", (int) color.getRed() * 255, (int) color.getGreen() * 255, (int) color.getBlue() * 255, (int)  color.getOpacity());
+        String hoverColorString = String.format("rgba(%d, %d, %d, %d)", (int) (color.getRed()), (int) (color.getGreen() * 255), (int) (color.getBlue() * 255), (int) (color.getOpacity() - 0.2));
 
         // Determine if the color is dark or light
         double luminance = 0.2126 * color.getRed() + 0.7152 * color.getGreen() + 0.0722 * color.getBlue();
@@ -26,7 +26,7 @@ public class FancyButton extends Button {
         this.setStyle(styleString);
 
         this.setOnMouseEntered(e -> {
-            this.setStyle(styleString + "-fx-background-color: " + hoverColorString + ";" + "-fx-border-color:" + textColor +" ;");
+            this.setStyle("-fx-font-size: 14px; -fx-background-color: " + hoverColorString + ";" + "-fx-border-color:" + textColor +" ;");
         });
 
         this.setOnMouseExited(e -> {
