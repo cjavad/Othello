f2952ab0b4febb9d777dd53e73df3c03f380819a
Magnus <88041689+MagnusAH@users.noreply.github.com>
2023-01-19 23:27:32 +0100

    Multiply color values by 255

--- src/java/othello/components/ui/FancyButton.java
+++ src/java/othello/components/ui/FancyButton.java
@@ -7,21 +7,13 @@ public class FancyButton extends Button {
     public FancyButton(String text, Color color) {
         super(text);
 
-        String colorString = String.format("rgb(%f, %f, %f, %f)", color.getRed(), color.getGreen(), color.getBlue(), color.getOpacity());
-        String hoverColorString = String.format("rgb(%f, %f, %f, %f)", color.getRed() + 20, color.getGreen() + 20, color.getBlue() + 20, color.getOpacity() - 0.2);
+        String colorString = String.format("rgba(%f, %f, %f, %f)", color.getRed() * 255, color.getGreen() * 255, color.getBlue() * 255, color.getOpacity());
+        String hoverColorString = String.format("rgba(%f, %f, %f, %f)", color.getRed() * 255 + 20, color.getGreen() * 255 + 20, color.getBlue() * 255 + 20, color.getOpacity() - 0.2);
 
         // Determine if the color is dark or light
         double luminance = 0.2126 * color.getRed() + 0.7152 * color.getGreen() + 0.0722 * color.getBlue();
         String textColor = luminance > 0.4 ? "#000000" : "#ffffff";
         String styleString = "-fx-background-color: "+ colorString +"; -fx-border-color: transparent;-fx-font-size: 14px; -fx-text-fill:" + textColor + ";";
-
-
-        System.out.println(text + " (" + color + ")");
-        System.out.println(colorString);
-        System.out.println("luminance = " + luminance);
-        System.out.println(textColor);
-        System.out.println(styleString);
-
 
         this.setStyle(styleString);
 
